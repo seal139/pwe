c1b7421fd8a7dba727928ea10eeb1fa9dc520fc0
Septian Pramana R. <septian13.ian@gmail.com>
2024-11-08 23:56:36 +0700

    Setup app config to be used by Apache HTTP production server

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/pwe/public';
+    public string $baseURL = 'http://localhost/';
 
 
     /**
